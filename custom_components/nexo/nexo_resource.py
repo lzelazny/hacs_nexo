4c759c176c3a7cd95a209fbeeb6178c54cfa6eca
Paweł <pawel@msd.info.pl>
2025-06-07 13:22:37 +0200

    Refactor: centralize common functionality in base classes

--- custom_components/nexo/nexo_resource.py
+++ custom_components/nexo/nexo_resource.py
@@ -1,37 +1,74 @@
-from re import S
-from typing import Callable, Final
+"""Nexo resource."""
+
 import asyncio
+from collections.abc import Callable
 import logging
+from typing import Final
+
+import websocket
 
 
 class NexoResource:
-    """Common Nexo features"""
+    """Base class for Nexo resources."""
+
     _LOGGER: Final = logging.getLogger(__name__)
-    def __init__(
-        self, web_socket, id, type, name, state, timestamp, *args, **kwargs
-    ) -> None:
-        self.web_socket = web_socket
-        self.id = id
-        self.type = type
-        self.name = name
+
+    def __init__(self, web_socket, id, name, state, *args, **kwargs) -> None:
+        """Initialize the Nexo resource."""
+        self.web_socket: websocket = web_socket
+        self._id = id
+        self._name = name
         self.state = state
-        self.timestamp = timestamp
         self._callbacks = set()
 
-    async def wrapper(self, awaitable):
-        return await awaitable()
+    @property
+    def id(self) -> str:
+        """Return the id of the resource."""
+        return self._id
 
-    def send(self, message) -> None:
-        """Send"""
-        self.web_socket.Send(message)
+    @property
+    def name(self) -> str:
+        """Return the name of the resource."""
+        return self._name
+
+    async def _wrapper(self, awaitable):
+        """Return the result of the awaitable."""
+        if awaitable is not None:
+            return await awaitable()
+        return None
+
+    async def _async_send(self, message) -> None:
+        """Send a message to the websocket."""
+        self.web_socket.send(message)
+
+    async def _async_send_cmd(self, cmd) -> None:
+        """Send a command message to the websocket."""
+        await self._async_send(f'{{"type":"resource","id":{self.id},"cmd":{{{cmd}}}}}')
+
+    async def _async_send_cmd_operation_custom(self, operation, **kwargs) -> None:
+        """Send a custom operation command message to the websocket."""
+        cmd = f'"operation":{operation}'
+        if kwargs:
+            cmd += f",{','.join(f'"{key}":{value}' for key, value in kwargs.items())}"
+        await self._async_send_cmd(cmd)
+
+    async def _async_send_cmd_operation(self, operation, value=None) -> None:
+        """Send an operation command message to the websocket."""
+        if value is None:
+            await self._async_send_cmd_operation_custom(operation)
+        else:
+            await self._async_send_cmd_operation_custom(operation, value=value)
 
     def register_callback(self, callback: Callable[[], None]) -> None:
+        """Register a callback to be called when the resource state changes."""
         self._callbacks.add(callback)
 
     def remove_callback(self, callback: Callable[[], None]) -> None:
+        """Remove a callback."""
         self._callbacks.discard(callback)
 
     def publish_update(self, _loop) -> None:
+        """Notify Home Assistant about a state change."""
         for callback in self._callbacks:
             self._LOGGER.debug(
                 "Notifying HA about state change of device id: %s type: %s to state %s",
@@ -39,4 +76,4 @@ class NexoResource:
                 self.name,
                 self.state,
             )
-            asyncio.run_coroutine_threadsafe(self.wrapper(callback), _loop)
+            asyncio.run_coroutine_threadsafe(self._wrapper(callback), _loop)
--- custom_components/nexo/switch.py
+++ custom_components/nexo/switch.py
@@ -1,15 +1,18 @@
-"""Support for Nexo Switches."""
-from __future__ import annotations
-import logging
-from typing import Any, Final
+"""Nexo Switch Entity."""
 
-from homeassistant.config_entries import ConfigEntry
+from __future__ import annotations
+
+import logging
+from typing import Final
+
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .nexo_output import NexoOutput
 from .const import DOMAIN
+from .nexo import HANexo
+from .nexo_output import NexoOutput
 
 _LOGGER: Final = logging.getLogger(__name__)
 
@@ -17,51 +20,30 @@ _LOGGER: Final = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
-    """Set up"""
+    """Set up."""
     nexo = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(HANexoOutput(switch) for switch in nexo.get_resources_by_type(NexoOutput))
+    async_add_entities(
+        HANexoSwitch(switch) for switch in nexo.get_resources_by_type(NexoOutput)
+    )
 
 
-class HANexoOutput(SwitchEntity):
-    """Home Assistant Nexo output"""
+class HANexoSwitch(HANexo, SwitchEntity):
+    """Home Assistant Nexo Switch."""
 
     def __init__(self, nexo_output) -> None:
-        super().__init__()
-        self._nexoOutput = nexo_output
-        self._name = nexo_output.name
-        self._attr_is_on = self._nexoOutput.is_on()
-
-    @property
-    def unique_id(self) -> str:
-        """Return the id of this Nexo output."""
-        return str(self._nexoOutput.id)
-
-    @property
-    def name(self) -> str:
-        """Return the display name of this output."""
-        return str(self._name)
+        """Initialize the Nexo switch."""
+        super().__init__(nexo_resource=nexo_output)
+        self._nexo_Output: NexoOutput = nexo_output
 
     @property
     def is_on(self) -> bool:
-        return self._nexoOutput.is_on()
+        """Return the state of the switch."""
+        return self._nexo_Output.is_on
 
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        self._nexoOutput.turn_on()
+    async def async_turn_on(self):
+        """Turn the entity on."""
+        await self._nexo_Output.async_turn_on()
 
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        self._nexoOutput.turn_off()
-
-    @callback
-    def async_update_state(self) -> None:
-        """Notify of internal state update"""
-        self.async_write_ha_state()
-
-    async def async_added_to_hass(self) -> None:
-        """Run when this Entity has been added to HA."""
-        # Nexo Light should also register callbacks to HA when their state changes
-        self._nexoOutput.register_callback(self.async_update_state)
-
-    async def async_will_remove_from_hass(self) -> None:
-        """Entity being removed from hass."""
-        # The opposite of async_added_to_hass. Remove any registered call backs here.
-        self._nexoOutput.remove_callback(self.async_update_state)
+    async def async_turn_off(self):
+        """Turn the entity off."""
+        await self._nexo_Output.async_turn_off()
